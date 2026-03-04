4a9d7b336a41676bc5b2b4983b20adcebbf95b76
Darya Vus <vus.dasha@gmail.com>
2026-03-04 09:07:26 +0300

    project initialization, structure creation, adding tailwindcss, react-hook-form and zod

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+import { Header } from './components/Header';
+import { Hero } from './components/Hero';
+import { About } from './components/About';
+import { Services } from './components/Services';
+import { Contact } from './components/Contact';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Services />
+        <Contact />
+      </main>
+    </>
+  );
+}
+
+export default App
