a044225d8b162a9976df2acb9c78a58d151dc9b1
Darya Vus <vus.dasha@gmail.com>
2026-03-04 12:59:22 +0300

    add About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,75 @@
+import aboutImage from '../assets/about-img.jpg';
+
 export const About = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section
+      id="about"
+      className="relative"
+      style={{
+        paddingLeft: '257px',
+        paddingRight: '253px',
+        paddingTop: '93px',
+        paddingBottom: '140px',
+      }}
+    >
+      <h2
+        className="font-alata text-marvel-black"
+        style={{
+          fontSize: '90px',
+          lineHeight: '100px',
+          marginBottom: '50px',
+        }}
+      >
+        About Us
+      </h2>
+
+      <div
+        className="bg-marvel-yellow"
+        style={{
+          width: '100px',
+          height: '6px',
+          marginBottom: '55px',
+        }}
+      />
+
+      <div
+        className="flex"
+        style={{
+          gap: '68px',
+        }}
+      >
+        <div
+          className="flex-1"
+          style={{
+            width: '486px',
+            height: '315px',
+          }}>
+          <img
+            src={aboutImage}
+            alt="About Marvel Power Group"
+            className="w-full h-auto"
+          />
+        </div>
+
+        <div
+          className="flex-1"
+          style={{
+            fontSize: '16px',
+            width: '490px',
+            height: '315px',
+            marginRight: '65px',
+          }}>
+          <p className="font-sofia text-marvel-black">
+            Our dynamic, solutions-oriented team brings decades of relevant transactional, analytical and regulatory experience to supporting our clients’ unique agendas.
+            <br />
+            <br />
+            We are experienced practitioners who have run companies, successfully originated and closed complex deals, advocated and negotiated favorable policy and advanced technological innovation and climate leadership.
+            <br />
+            <br />
+            We specialize in structuring balanced solutions for all affected stakeholders and uncovering economic opportunities in otherwise opaque environments.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
